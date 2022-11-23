a6e4f98188a63068658a784188e240a25d46bb2d
Aurimas <vajkis1@gmail.com>
2022-11-23 15:45:31 +0200

    table body

--- src/components/TableBody.jsx
+++ src/components/TableBody.jsx
@@ -1,9 +1,25 @@
+import { useContext } from "react";
+import DataContext from "./DataContext";
+
 function TableBody() {
+
+    const { data } = useContext(DataContext);
+
     return (
         <tbody>
-
+            {data?.map(e => {
+                console.log(e.check);
+                return (
+                    <tr key={e.id}>
+                        <td><input type='checkbox' onChange={() => { }} checked={e.check}></input></td>
+                        <td>{e.name}</td>
+                        <td>{e.age}</td>
+                        <td>{e.city}</td>
+                    </tr>
+                )
+            })}
         </tbody>
-    )
+    );
 }
 
 export default TableBody;
