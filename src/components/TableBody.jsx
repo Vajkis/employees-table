7f372a4079eb6a103615f888f8b4599af455a6bb
Aurimas <vajkis1@gmail.com>
2022-11-24 20:03:05 +0200

    checkbox refactoring

--- src/components/TableBody.jsx
+++ src/components/TableBody.jsx
@@ -11,7 +11,7 @@ function TableBody() {
                 console.log(e.check);
                 return (
                     <tr key={e.id}>
-                        <td><input type='checkbox' onChange={() => { }} checked={e.check}></input></td>
+                        <td><input type='checkbox' onChange={e => { }} checked={e.check}></input></td>
                         <td>{e.name}</td>
                         <td>{e.age}</td>
                         <td>{e.city}</td>
