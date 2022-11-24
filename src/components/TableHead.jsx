e0c7b59c542b68531b8ecb5ed8d7a53eabc5402f
Aurimas <vajkis1@gmail.com>
2022-11-24 19:39:59 +0200

    refactoring

--- src/components/TableHead.jsx
+++ src/components/TableHead.jsx
@@ -18,14 +18,17 @@ function TableHead() {
     }, [data]);
 
     const check = e => {
-        const v = e.target.checked;
-        setIsCheck(v);
+        const c = e.target.checked;
+        setIsCheck(c);
     }
 
     return (
         <thead>
             <tr>
-                <th><input type='checkbox' onChange={e => { check(e); dispachData(checkAll_action(e.target.checked)); }} checked={isCheck}></input></th>
+                <th><input type='checkbox' checked={isCheck} onChange={e => {
+                    check(e);
+                    dispachData(checkAll_action(e.target.checked));
+                }} ></input></th>
                 <th>Name</th>
                 <th>Age</th>
                 <th>City</th>
