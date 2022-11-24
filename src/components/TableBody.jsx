58f2162d45fc5506a8d5b970d32779879dc875e0
Aurimas <vajkis1@gmail.com>
2022-11-24 21:25:09 +0200

    employee checkbox refactoring

--- src/components/TableBody.jsx
+++ src/components/TableBody.jsx
@@ -6,12 +6,20 @@ function TableBody() {
 
     const { data, dispachData } = useContext(DataContext);
 
+    const check = (id, e) => {
+        const c = e.target.checked;
+        dispachData(checkEmployee_action(id, c));
+
+
+    }
+
+
     return (
         <tbody>
             {data?.map(e => {
                 return (
                     <tr key={e.id}>
-                        <td><input type='checkbox' onChange={event => dispachData(checkEmployee_action(e.id, event.target.checked))} checked={e.check}></input></td>
+                        <td><input type='checkbox' onChange={event => check(e.id, event)} checked={e.check}></input></td>
                         <td>{e.name}</td>
                         <td>{e.age}</td>
                         <td>{e.city}</td>
