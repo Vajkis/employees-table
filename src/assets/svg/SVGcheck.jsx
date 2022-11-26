954ade1673a7e8a05405d3e73c323e993e4fe35e
Aurimas <vajkis1@gmail.com>
2022-11-26 02:01:17 +0200

    check mark component to svg

--- src/assets/svg/SVGcheck.jsx
+++
@@ -1,9 +0,0 @@
-function SVGcheck({ className, style }) {
-    return (
-        <svg className={className} style={style} fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="5" d="M6,13 l5,4 l9,-12" />
-        </svg>
-    )
-}
-
-export default SVGcheck;
