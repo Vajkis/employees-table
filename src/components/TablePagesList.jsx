78803b350eee37aad67366b26ff08d6289839a77
Aurimas <vajkis1@gmail.com>
2022-11-29 22:10:59 +0200

    hardcoded style in TablePagesList component

--- src/components/TablePagesList.jsx
+++ src/components/TablePagesList.jsx
@@ -1,14 +1,14 @@
 function TablePagesList() {
     return (
-        <nav style={{ display: 'flex', gap: '10px' }}>
-            <button>{'<'}</button>
-            <ul style={{ display: 'flex', listStyleType: 'none', gap: '10px' }}>
+        <nav style={{ display: 'flex', gap: '10px', width: '100%', justifyContent: 'center', marginTop: '10px', alignItems: 'center' }}>
+            <button style={{ aspectRatio: '1 / 1' }}>{'<'}</button>
+            <ul style={{ display: 'flex', listStyleType: 'none', gap: '10px', padding: 0 }}>
                 <li>1</li>
                 <li>2</li>
                 <li>...</li>
                 <li>4</li>
             </ul>
-            <button>{'>'}</button>
+            <button style={{ aspectRatio: '1 / 1' }}>{'>'}</button>
         </nav>
     )
 }
