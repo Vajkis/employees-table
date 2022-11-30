2fab039b3c81489452cf7e4eee139e3f5d2986f9
Aurimas <vajkis1@gmail.com>
2022-11-30 23:02:01 +0200

    test tool

---
+++ src/functions/getRandomEmployee.js
@@ -0,0 +1,18 @@
+import getId from "./getId";
+import randName from "./randName";
+import randNum from "./randNum";
+
+function getRandomEmployee() {
+
+    return {
+        id: getId(),
+        name: randName(),
+        age: randNum(18, 62),
+        city: ['Vilnius', 'Kaunas', 'Klaipėda'][randNum(0, 2)],
+        deleted: false,
+        focus: false,
+        check: false
+    }
+}
+
+export default getRandomEmployee;
