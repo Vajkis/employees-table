a766a70da9855527bd5233fef55b55ba0b9553e7
Aurimas <vajkis1@gmail.com>
2022-11-30 15:02:13 +0200

    page list css

--- src/components/TablePagesList.jsx
+++ src/components/TablePagesList.jsx
@@ -14,8 +14,8 @@ function TablePagesList() {
     }, [pageSize, dispachData]);
 
     return (
-        <div style={{ position: 'relative', marginTop: '10px' }}>
-            <select style={{ position: 'absolute', top: '50%', transform: 'translateY(-50%)' }} value={pageSize} onChange={e => setPageSize(e.target.value)}>
+        <div className='page-list'>
+            <select value={pageSize} onChange={e => setPageSize(e.target.value)}>
                 <option value=''>Page size</option>
                 <option value='10'>10</option>
                 <option value='25'>25</option>
@@ -23,15 +23,15 @@ function TablePagesList() {
                 <option value='100'>100</option>
             </select>
 
-            <nav style={{ display: 'flex', gap: '10px', width: '100%', justifyContent: 'center', alignItems: 'center' }}>
-                <button style={{ aspectRatio: '1 / 1' }}>{'<'}</button>
-                <ul style={{ display: 'flex', listStyleType: 'none', gap: '10px', padding: 0 }}>
-                    <li>1</li>
+            <nav>
+                <button>{'<'}</button>
+                <ul>
+                    <li className='current-page'>100</li>
                     <li>2</li>
-                    <li>...</li>
+                    <li className='space'>...</li>
                     <li>4</li>
                 </ul>
-                <button style={{ aspectRatio: '1 / 1' }}>{'>'}</button>
+                <button>{'>'}</button>
             </nav>
         </div>
     );
